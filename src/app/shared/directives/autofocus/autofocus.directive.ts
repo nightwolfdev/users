5a54a3a4fbefab60154493ac7ad9713cca65e0e0
NightWolf Software Development <nightwolfdevelopment@gmail.com>
2022-09-14 13:16:34 -0400

    Update autofocus directive

--- src/app/shared/directives/autofocus/autofocus.directive.ts
+++ src/app/shared/directives/autofocus/autofocus.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]'
 })
-export class AutofocusDirective {
+export class AutofocusDirective implements AfterViewInit {
   constructor(private element: ElementRef<HTMLInputElement>) { }
 
   ngAfterViewInit(): void {
